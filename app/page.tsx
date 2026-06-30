d67e31a3c34670bb61e186e29686ef5085e0cb0d
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-30 09:24:53 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,17 @@ import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import HowItWorks from "@/components/HowItWorks";
 import Statistics from "@/components/Statistics";
+import FAQ from "@/components/FAQ";
 
 export default function Home() {
   return (
-    <>
+   <>
   <Navbar />
   <Hero />
   <Features />
   <HowItWorks />
   <Statistics />
+  <FAQ />
 </>
   );
 }
